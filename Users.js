0a2066e8477ecf0eff304b43da104cb54ae7ce04
CelloHarri <juliuszharrison@gmail.com>
2026-04-05 10:54:25 -0600

    Fixed Users.js connecting to mongoose twice

--- Users.js
+++ Users.js
@@ -4,15 +4,6 @@ var bcrypt = require('bcrypt-nodejs');
 
 mongoose.Promise = global.Promise;
 
-//mongoose.connect(process.env.DB, { useNewUrlParser: true });
-try {
-    mongoose.connect(process.env.DB, { useNewUrlParser: true, useUnifiedTopology: true }, () =>
-        console.log("connected"));
-} catch (error) {
-    console.log("could not connect");
-}
-mongoose.set('useCreateIndex', true);
-
 //user schema
 var UserSchema = new Schema({
     name: String,
@@ -35,12 +26,14 @@ UserSchema.pre('save', function (next) {
     });
 });
 
-UserSchema.methods.comparePassword = function (password, callback) {
+UserSchema.methods.comparePassword = function (password) {
     var user = this;
-
-    bcrypt.compare(password, user.password, function (err, isMatch) {
-        callback(isMatch);
-    })
+    return new Promise(function (resolve, reject) {
+        bcrypt.compare(password, user.password, function (err, isMatch) {
+            if (err) return reject(err);
+            resolve(isMatch);
+        });
+    });
 }
 
 //return the model to server
