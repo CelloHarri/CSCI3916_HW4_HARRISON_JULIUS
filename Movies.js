ec65e14b973249dae70aead16877339ce84862b2
CelloHarri <juliuszharrison@gmail.com>
2026-04-05 10:41:26 -0600

    made title unique

--- Movies.js
+++ Movies.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 
 // Movie schema
 const MovieSchema = new mongoose.Schema({
-    title: { type: String, required: true, index: true },
+    title: { type: String, required: true, index: true, unique: true },
     releaseDate: { type: Number, min: [1900, 'Must be greater than 1899'], max: [2100, 'Must be less than 2100'] },
     genre: {
         type: String,
